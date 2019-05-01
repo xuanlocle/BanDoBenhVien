edc9278d8eb6db322ba3cbff4a858c8a7ac61e38
XuanLoc <xuanlocle2412@gmail.com>
2019-04-30 22:14:05 -0700

    Xong trang chủ

--- app/src/main/java/com/xuanlocle/bandobenhvien/MainActivity.java
+++ app/src/main/java/com/xuanlocle/bandobenhvien/MainActivity.java
@@ -3,8 +3,11 @@ package com.xuanlocle.bandobenhvien;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
@@ -28,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
     ScrollView mainScrollView;
     ArrayList<String> lstPhongBan;
     TextView tvGet;
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private FirstFragment fstFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,16 +43,72 @@ public class MainActivity extends AppCompatActivity {
 //        Toolbar toolbar = findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
-        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-
+        fstFragment = new FirstFragment();
         //anh xa
-        mainScrollView = (ScrollView)findViewById(R.id.scrollvmain);
-        tvGet = (TextView)findViewById(R.id.tvGet);
-//        mainScrollView.fullScroll(ScrollView.FOCUSABLES_ALL);
-        mainScrollView.setFocusableInTouchMode(true);
-        mainScrollView.setDescendantFocusability(ViewGroup.FOCUS_BEFORE_DESCENDANTS);
+//        mainScrollView = (ScrollView)findViewById(R.id.scrollvmain);
 
+
+
+////        mainScrollView.fullScroll(ScrollView.FOCUSABLES_ALL);
+//        mainScrollView.setFocusableInTouchMode(true);
+//        mainScrollView.setDescendantFocusability(ViewGroup.FOCUS_BEFORE_DESCENDANTS);
+//        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
         String URL = "http://210.245.111.217:480/api/values?id=1";
+        GetJSONFromServer(URL);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void setupTabIcons() {
+        TextView tabOne = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabOne.setText("Trang chủ");
+        tabOne.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.baseline_home_white_18, 0, 0);
+        tabLayout.getTabAt(0).setCustomView(tabOne);
+
+        TextView tabTwo = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabTwo.setText("Đăng ký khám");
+        tabTwo.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.baseline_assignment_white_18, 0, 0);
+        tabLayout.getTabAt(1).setCustomView(tabTwo);
+
+        TextView tabThree = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabThree.setText("Bản đồ");
+        tabThree.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.baseline_place_white_18, 0, 0);
+        tabLayout.getTabAt(2).setCustomView(tabThree);
+
+        TextView tabFour = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+        tabFour.setText("Cá nhân");
+        tabFour.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.baseline_face_white_18, 0, 0);
+        tabLayout.getTabAt(3).setCustomView(tabFour);
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(fstFragment, "Trang chủ");
+        adapter.addFragment(new FirstFragment(), "Đăng ký khám");
+        adapter.addFragment(new FirstFragment(), "Bản đồ");
+        adapter.addFragment(new FirstFragment(), "Cá nhân");
+        viewPager.setAdapter(adapter);
+    }
+
+    private void GetJSONFromServer(String URL){
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
         lstPhongBan = new ArrayList<String>();
         JsonArrayRequest jarr = new JsonArrayRequest(Request.Method.GET, URL, null, new Response.Listener<JSONArray>() {
             @Override
@@ -56,8 +120,8 @@ public class MainActivity extends AppCompatActivity {
                         JSONObject job = response.getJSONObject(i);
                         String name = job.getString("TenPhongBan");
                         lstPhongBan.add(name);
-                        tvGet.setText(tvGet.getText()+"\n"+name);
-                        Toast.makeText(MainActivity.this, name , Toast.LENGTH_SHORT).show();
+                        fstFragment.setTextView(fstFragment.getTextView()+"\n"+name);
+//                        Toast.makeText(MainActivity.this, name , Toast.LENGTH_SHORT).show();
                     }
 
                 } catch (Exception e){
@@ -73,16 +137,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         queue.add(jarr);
-
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
