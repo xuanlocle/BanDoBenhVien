b4624ecaff16ec6c276d787565dcf00cc30bfbff
XuanLoc <xuanlocle2412@gmail.com>
2019-05-13 00:02:16 +0700

    Xong giao diện đăng nhập và code qr

--- app/src/main/java/com/xuanlocle/bandobenhvien/account/LoginActivity.java
+++ app/src/main/java/com/xuanlocle/bandobenhvien/account/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.xuanlocle.bandobenhvien.account;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +11,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.xuanlocle.bandobenhvien.MainActivity;
 import com.xuanlocle.bandobenhvien.R;
 
@@ -20,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        final Activity activity = this;
         btnDangNhap = (Button)findViewById(R.id.btnDangNhap);
         btnDangNhapFacebook = (Button)findViewById(R.id.btnDangNhapFacebook);
         btnDangNhapBangTheBaoHiem = (Button)findViewById(R.id.btnDangNhapBangTheBaoHiem) ;
@@ -50,10 +56,32 @@ public class LoginActivity extends AppCompatActivity {
         btnDangNhapBangTheBaoHiem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Quét mã QR trên thẻ bảo hiểm của bạn.");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
             }
         });
 
 
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result!=null){
+            if(result.getContents()==null)
+                Toast.makeText(this, "Không quét được mã!", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_SHORT).show();
+        }
+
+
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
